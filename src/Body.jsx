b2318b0ca2a644808bf0318315b3b58bb00eb899
Vishnu3522 <vishnukumar.it24@jecrc.ac.in>
2025-05-03 16:13:01 +0530

    again

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import axios from 'axios';
